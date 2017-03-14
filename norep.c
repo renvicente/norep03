b3f28a5e96ee71b9a152a2ac8d7b1e063bf951d2
Renan Vicente Da Silva <renanvicente17@hotmail.com>
2017-03-14 13:45:16 -0300

    Criado esqueleto do programa

---
+++ norep.c
@@ -0,0 +1,17 @@
+/* Programa norep.c
+ * Imprime 100 linhas, cada uma contendo numeros aleatorios
+ * de 0 a 5 sem repeticao
+ * 
+ * Autor:   Renan Vicente
+ * Data:    14/03/2017
+ */
+
+#include <stdio.h>
+#include <time.h>
+
+main(void)
+{
+
+}
+
+
