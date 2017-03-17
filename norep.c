5339bb543a88fa3783655940a785cb03d0050184
Renan Vicente Da Silva <renanvicente17@hotmail.com>
2017-03-17 11:45:56 -0300

    Apagadas linhas de codigo desnecessarias

--- norep.c
+++ norep.c
@@ -33,9 +33,7 @@ int main(void)
     for(j=0; j<LINHAS; j++) /* controla quantas linhas serao processadas e impressas */
     {
         for(k=0; k<MAX; k++)
-        {
             sorteados[k] = MAX+1; /* preenche a caixa de numeros com valores fora do intervalo de valores sorteados */
-        }
         printf("\n");
         contador = 0; /* zera a quantidade de numeros sorteados para iniciar uma nova linha */
         do
@@ -43,20 +41,12 @@ int main(void)
             t = sortear();
             /* printf("Numero sorteado: %d\n", t); */
             for(k=0; k<MAX; k++)
-            {
                 if(t == k)
-                {
                     quantidade[k] += 1; /* incrementa a quantidade de vezes que cada numero e' sorteado */ 
-                }
-            }
             verifica = 0; /* usada para verificar se um numero ja foi sorteado em uma linha */
             for(k=0; k<MAX; k++)
-            {
                 if(t == sorteados[k]) /* se o numero que foi sorteado agora e' igual a um dos numeros ja sorteados na linha atual */
-                {
                     verifica = 1; /* atribue 1 para nao entrar na condicao abaixo */
-                }
-            }
             if(!verifica) /* se <verifica> ainda estiver igual a zero, significa que o numero sorteado nao e repetido, entao sera aceito" */
             {
                 sorteados[contador] = t; /* o numero e guardado no vetor de numeros desta linha */
@@ -71,9 +61,7 @@ int main(void)
         printf("\n");
     }
     for(k=0; k<MAX; k++)
-    {
         somatorio += quantidade[k]; /* faz o somatorio da quantidade de sorteios de todos os numeros */
-    }
     for(k=0; k<MAX; k++)
     {
         p = (quantidade[k]*100)/somatorio;
@@ -88,3 +76,4 @@ int sortear(void)
     x = rand() % MAX;
         return x;
 }
+
